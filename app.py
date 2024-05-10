44a25abb9f4184acba93847563ea32d36ec5a5de
von-fss <felipe.ruiz.s@gmail.com>
2024-05-10 18:55:00 -0300

    Removing comments

--- app.py
+++ app.py
@@ -27,27 +27,3 @@ def getAll():
         downloadFile = download.downloadFiles()
         downloadFile.download(fileName)
     return "end"
-
-#@app.route('/getComercio', methods=['GET'])
-#def getComercio():
-#     arch = pd.read_csv("arquivos/Comercio.csv", sep = ";")
-#     arch.to
-#     return arch.to_json()
-
-# @app.route('/getExp', methods=['GET'])
-# def getExp():
-#     arquivo = request.args.get('arquivo')
-#     arch = pd.read_csv("arquivos/Exp" + arquivo + ".csv", sep = ";")
-#     return arch.to_json()
-
-# @app.route('/getImp', methods=['GET'])
-# def getImp():
-#     arquivo = request.args.get('arquivo')
-#     arch = pd.read_csv("arquivos/Imp" + arquivo + ".csv", sep = ";")
-#     return arch.to_json()
-
-# @app.route('/getProcessamento', methods=['GET'])
-# def getProcessamento():
-#     arquivo = request.args.get('arquivo')
-#     arch = pd.read_csv("arquivos/Processa" + arquivo + ".csv", sep = ";")
-#     return arch.to_json()
