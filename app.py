30de5eff73d91be35d283a9e283652a0a68da765
von-fss <felipe.ruiz.s@gmail.com>
2024-05-19 04:02:33 -0300

    scrapper v1 -> stable

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from helpers.fastapiconf import initFast
 import authentication as auth
 from routers.authentication import router as auth_route
 from routers.embrapa import router as embrapa_route
+from routers.embrapaScrapper import router as embrapa_scrapper_route
 
 app = initFast()
 
 app.include_router(auth_route)
 app.include_router(embrapa_route, dependencies=[Depends(auth.oauth2_scheme)])
+app.include_router(embrapa_scrapper_route, dependencies=[Depends(auth.oauth2_scheme)])
 
 @app.get('/', include_in_schema=False)
 async def docs() -> RedirectResponse:
--- routers/embrapa.py
+++ routers/embrapa.py
@@ -10,14 +10,17 @@ router = APIRouter(
 @router.get('/listFiles', tags=["Download Embrapa"])
 def lista_arquivos_da_embrapa():
     """
-    Lista todos os arquivos da Embrapa com o seu respectivo ID
+    Lista todos os arquivos da Embrapa com o seu respectivo ID. Ids usados no download na rota /getFile
     """
     return JSONResponse(content=jsonable_encoder(get_index_filename()), status_code = status.HTTP_201_CREATED)
 
 @router.get('/getFile', tags=["Download Embrapa"])
 def download_de_arquivo(index: int | None = None):
     """
-    Faz o download de um arquivo da Embrapa com base no seu id de referência (index)
+    Faz o download de um arquivo da Embrapa com base no seu id de referência (index) ou o download de todos arquivos caso o parametro index não seja informado.
+    
+    Args:
+        index (int) Opcional: índice do arquivo a ser feito o download. Pode ser obtido pela rota /listFiles
     """
     try:
         embrapa_download(index)
