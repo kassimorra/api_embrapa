bf01559e6f9456d77051ef10a46dabd09328db4b
Kassim <kassimorra@gmail.com>
2024-05-28 20:21:09 -0300

    adjust etl

--- routers/etl.py
+++ routers/etl.py
@@ -4,20 +4,21 @@ from helpers.etlFiles import etlFiles
 router = APIRouter(
     prefix="/etl"
 )
+etl = etlFiles()
 
 @router.get('/getFile/<index>', tags=["Download Embrapa and Make ETL in Files"])
 def Get_Single_File(index: int):
-    etlFiles().makeEtl(index)
-    # try:
-    #     etlFiles.makeEtl(index)
-    # except:
-    #     raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Failed to download file")
-    # return {"message": "sucesso"}
-    
-@router.get('/getFile', tags=["Download Embrapa and Make ETL in Files"])
-def Get_All_Files():
     try:
-        etlFiles.makeEtl(-1)
+        etl.makeEtl(index)
     except:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Failed to download file")
     return {"message": "sucesso"}
+    
+@router.get('/getFile', tags=["Download Embrapa and Make ETL in Files"])
+def Get_All_Files():
+    etl.makeEtl(-1)
+    """try:
+        etl.makeEtl(-1)
+    except:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Failed to download file")
+    return {"message": "sucesso"}"""
